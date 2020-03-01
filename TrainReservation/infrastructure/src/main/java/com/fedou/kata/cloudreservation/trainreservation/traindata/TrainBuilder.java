a76357607d799cdc93700608555d03c1d033deb3
Nicolas Fédou <nicolas.fedou@gmail.com>
2020-03-01 14:14:01 +0100

    passage du code d'intégration de trains avec des places a des trins avec des places libres et des places prises.

--- TrainReservation/infrastructure/src/main/java/com/fedou/kata/cloudreservation/trainreservation/traindata/TrainBuilder.java
+++ TrainReservation/infrastructure/src/main/java/com/fedou/kata/cloudreservation/trainreservation/traindata/TrainBuilder.java
@@ -3,9 +3,11 @@ package com.fedou.kata.cloudreservation.trainreservation.traindata;
 import java.util.*;
 
 import static java.util.Arrays.stream;
+import static java.util.Collections.emptyList;
 
 public class TrainBuilder {
-    private Map<String, List<Integer>> coachesBuilder = new HashMap<>();
+    private Map<String, List<Integer>> freeSeatsCoachesBuilder = new HashMap<>();
+    private Map<String, Integer> totalSeatsCoachesBuilder = new HashMap<>();
     private String trainId;
 
     public TrainBuilder(String trainId) {
@@ -18,21 +20,34 @@ public class TrainBuilder {
     }
 
     public TrainBuilder with(SeatData seat) {
-        if(seat.bookingReference == null) {
+        if (seat.bookingReference == null) {
             addFreeSeat(seat.coach, seat.seatNumber);
         }
+        incrementTotalSeat(seat.coach);
         return this;
     }
 
+    private void incrementTotalSeat(String coach) {
+        Integer totalSeats = 1 + totalSeatsCoachesBuilder.getOrDefault(coach, 0);
+        totalSeatsCoachesBuilder.put(coach, totalSeats);
+    }
+
     private void addFreeSeat(String coach, int seatNumber) {
-        List<Integer> seats = coachesBuilder.computeIfAbsent(coach, key -> new LinkedList<>());
+        List<Integer> seats = freeSeatsCoachesBuilder.computeIfAbsent(coach, key -> new LinkedList<>());
         seats.add(seatNumber);
     }
 
     public Train build() {
         ArrayList<Coach> coaches = new ArrayList<>();
-        this.coachesBuilder.forEach(
-                (key, value) -> coaches.add(new Coach(key, value.size(), value)));
+
+        this.totalSeatsCoachesBuilder.forEach(
+                (key, value) -> coaches.add(
+                        new Coach(key,
+                                value,
+                                freeSeatsCoachesBuilder.getOrDefault(key, emptyList())
+                        )
+                )
+        );
         return new Train(trainId, coaches);
     }
 }
